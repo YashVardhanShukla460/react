23e4bf1687d67aee06dd899e0c2f6712b357179b
Yash <yashtopfragger@gmail.com>
2025-12-04 20:16:51 +0530

    move checkout page

--- Ecommerce-Project/src/App.jsx
+++ Ecommerce-Project/src/App.jsx
@@ -1,12 +1,13 @@
 import { HomePage } from './pages/HomePage'
 import { Routes, Route } from 'react-router'
+import { CheckoutPage } from './pages/CheckoutPage'
 import './App.css'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/checkout" element={<div>Hola</div>} />
+      <Route path="/checkout" element={<CheckoutPage />} />
     </Routes>
   )
 }
