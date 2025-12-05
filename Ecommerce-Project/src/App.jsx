1e53f49944af091a78ff36aa1ace401f51e78ce1
Yash <yashtopfragger@gmail.com>
2025-12-06 00:41:48 +0530

    lift cart up and share it between pages

--- Ecommerce-Project/src/App.jsx
+++ Ecommerce-Project/src/App.jsx
@@ -1,19 +1,28 @@
-import { HomePage } from './pages/HomePage'
-import { Routes, Route } from 'react-router'
-import { CheckoutPage } from './pages/CheckoutPage/CheckoutPage'
-import { OrdersPage } from './pages/OrdersPage'
-import { TrackingPage } from './pages/TrackingPage'
-import './App.css'
+import { HomePage } from "./pages/HomePage";
+import { Routes, Route } from "react-router";
+import { CheckoutPage } from "./pages/CheckoutPage/CheckoutPage";
+import { OrdersPage } from "./pages/OrdersPage";
+import { TrackingPage } from "./pages/TrackingPage";
+import "./App.css";
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [cart, setCart] = useState([]);
+  useEffect(() => {
+    axios.get("/api/cart-items").then((response) => {
+      setCart(response.data);
+    });
+  }, []);
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/" element={<HomePage cart={cart} />} />
+      <Route path="/checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="orders" element={<OrdersPage />} />
       <Route path="tracking" element={<TrackingPage />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
